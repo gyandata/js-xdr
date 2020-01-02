3c68dbd1d849f05221da5099af332a839b77a15c
K J Venkatesh <kjvenky@gmail.com>
2020-01-02 12:39:58 +0530

    Removed linting in build:node

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,8 @@ gulp.task('lint:test', function lintTest() {
 
 gulp.task(
   'build:node',
-  gulp.series('lint:src', function buildNode() {
+  gulp.series(function buildNode() {
+    //'lint:src',
     return gulp
       .src('src/**/*.js')
       .pipe(plugins.babel())
